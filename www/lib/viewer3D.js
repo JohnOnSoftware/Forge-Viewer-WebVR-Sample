9150f3de689453972e418f0ddb8fbc7baf64745e
Zhong Wu <wuzhong@domiler.com>
2017-08-21 17:11:48 +0800

    update

--- www/lib/viewer3D.js
+++ www/lib/viewer3D.js
@@ -77358,19 +77358,22 @@ var av = Autodesk.Viewing,
      */
     avp.loadDependency = function(libNamespace, libName, callback, onError, amdName) {
         if (typeof window[libNamespace] === "undefined") {
-            if(libNamespace === "VRFrameData")
-                alert("Your browser does not support WebVR natively, try to load" + libName)
+            alert( libNamespace + " is not defined")
             var s = document.createElement("SCRIPT");
             s.src = libName.indexOf('://') > 0 ? libName : avp.getResourceUrl(libName);
+            alert(s.src)
             var clearCallbacks = function() {
                 s.onerror = null;
                 s.onload = null;
             };
             var errCallback = function() {
+                alert("failed to load script" + libName)
+                
                 clearCallbacks();
                 onError && onError();
             };
             var successCallback = function() {
+                alert("successfully loaded script" + libName)
                 clearCallbacks();
                 //if there is a dependency which use amd and we are running on amd environment we load it through require
                 if(typeof define === 'function' && define.amd && typeof require === 'function' && amdName) {
@@ -77387,6 +77390,7 @@ var av = Autodesk.Viewing,
             document.head.appendChild(s);
         }
         else if (callback){
+            alert( libNamespace + " is already defined")
             callback();
         }
     };
